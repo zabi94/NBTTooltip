bbf583ba7e1c449b6b89ff41703ce4ed4a6d8d21
Simone Zanin <zabi94@users.noreply.github.com>
2021-03-20 17:37:13 +0100

    Fix missing tail digits
    Closes #11

--- src/main/java/zabi/minecraft/nbttooltip/parse_engine/JsonParser.java
+++ src/main/java/zabi/minecraft/nbttooltip/parse_engine/JsonParser.java
@@ -21,7 +21,7 @@ public class JsonParser implements NbtTagParser {
 			list.add(new LiteralText("{}"));
 		} else {
 			list.add(new LiteralText("{"));
-			unwrapTag(list, tag, "", "", ModConfig.INSTANCE.compress?"":"  ");
+			unwrapTag(list, tag, ModConfig.INSTANCE.compress?"":"  ", "", ModConfig.INSTANCE.compress?"":"  ");
 			list.add(new LiteralText("}"));
 		}
 	}
@@ -75,15 +75,21 @@ public class JsonParser implements NbtTagParser {
 	}
 	
 	private static void addNamedValueToTooltip(List<Text> tooltip, Tag nbt, String name, String pad, boolean addComma) {
-		String cleanString = nbt instanceof StringTag ? escapeChars(nbt.toString()) : nbt.toString().substring(0, nbt.toString().length() - 1);
+		String cleanString = nbt instanceof StringTag ? escapeChars(nbt.toString()) : stripTypeIdentifiers(nbt.toString());
 		tooltip.add(new LiteralText(pad+'"'+name+"\": "+cleanString+(addComma ? "," : "")));
 	}
 	
 	private static void addValueToTooltip(List<Text> tooltip, Tag nbt, String name, String pad, boolean addComma) {
-		String cleanString = nbt instanceof StringTag ? escapeChars(nbt.toString()) : nbt.toString().substring(0, nbt.toString().length() - 1);
+		String cleanString = nbt instanceof StringTag ? escapeChars(nbt.toString()) : stripTypeIdentifiers(nbt.toString());
 		tooltip.add(new LiteralText(pad+cleanString+(addComma ? "," : "")));
 	}
 	
+	private static String stripTypeIdentifiers(String string) {
+		char last = string.charAt(string.length() - 1);
+		if (last >= '0' && last <= '9') return string;
+		return string.substring(0, string.length() - 1);
+	}
+
 	private static String escapeChars(String in) {
 		if ((in.charAt(0) == '"' && in.charAt(in.length()-1) == '"') || (in.charAt(0) == '\'' && in.charAt(in.length()-1) == '\'')) {
 			in = in.substring(1, in.length() - 1);
