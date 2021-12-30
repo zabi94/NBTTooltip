c680ad57e76498f6c9f97138de24fd4b06a4bf49
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2021-12-30 17:49:09 -0500

    update to 1.18

--- src/main/java/zabi/minecraft/nbttooltip/NBTTooltip.java
+++ src/main/java/zabi/minecraft/nbttooltip/NBTTooltip.java
@@ -149,7 +149,7 @@ public class NBTTooltip implements ClientModInitializer {
 			} else {
 				list.add(new LiteralText(""));
 			}
-			NbtCompound tag = stack.getTag();
+			NbtCompound tag = stack.getNbt();
 			ArrayList<Text> ttip = new ArrayList<>(lines);
 			if (tag!=null) {
 				if (ModConfig.INSTANCE.showDelimiters) {
@@ -189,7 +189,7 @@ public class NBTTooltip implements ClientModInitializer {
 		StringBuilder sb = new StringBuilder();
 		String name = I18n.translate(stack.getTranslationKey());
 		ArrayList<Text> nbtData = new ArrayList<>();
-		getCopyingEngine().parseTagToList(nbtData, stack.getTag(), false);
+		getCopyingEngine().parseTagToList(nbtData, stack.getNbt(), false);
 		nbtData.forEach(t -> {
 			sb.append(t.asString().replaceAll("§[0-9a-gk-or]", ""));
 			sb.append("\n");
